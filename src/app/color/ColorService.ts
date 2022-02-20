8ba6a121471bf2a1e55a72226fed52f1976264a5
Benedikt Lüken-Winkels <belu02@dfki.de>
2022-02-20 16:20:34 +0100

    Fix Factory

--- src/app/color/ColorService.ts
+++ src/app/color/ColorService.ts
@@ -1,12 +1,14 @@
 import { Pattern } from './patterns'
 
 export const PatternFactory = (pattern: Pattern): Pattern[] => {
-  return Object.values(pattern).map((value, i) => ({
-    a: value,
-    b: value,
-    c: value,
-    d: value,
-    single: true,
-    id: pattern.id + i,
-  }))
+  return Object.values(pattern)
+    .filter((value) => value.color)
+    .map((value, i) => ({
+      a: value,
+      b: value,
+      c: value,
+      d: value,
+      single: true,
+      id: pattern.id + i,
+    }))
 }
