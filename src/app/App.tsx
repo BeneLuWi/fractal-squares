33f6b001d0629d88234595e995e076b55e4a5394
Benedikt Lüken-Winkels <belu02@dfki.de>
2022-02-20 11:33:59 +0100

    Add Button Bar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,25 @@
 import React from 'react'
 import Square from './square/Square'
 import TreeProvider from './tree/TreeProvider'
-import ResetSquare from './square-control/ResetSquare'
+import SquareControl from './square-control/SquareControl'
 
 const App: React.FunctionComponent = () => {
   return (
     <TreeProvider>
-      <div className='bg-light w-100'>
-        <div className='vh-100 bg-white m-auto d-flex flex-column justify-content-between' style={{ maxWidth: 500 }}>
-          <div className='display-2'>Fractal Squares</div>
+      <div
+        className='bg-light w-100'
+        style={{
+          background: 'radial-gradient(circle, rgba(113,113,113,1) 0%, rgba(61,61,61,1) 100%)',
+        }}
+      >
+        <div
+          className='vh-100 m-auto d-flex flex-column justify-content-between overflow-hidden'
+          style={{ maxWidth: 500 }}
+        >
+          <div className='display-2 text-white-50'>Fractal Squares</div>
           <Square path={[]} />
-          <ResetSquare />
+
+          <SquareControl />
         </div>
       </div>
     </TreeProvider>
--- src/app/square-control/ResetSquare.tsx
+++ src/app/square-control/ResetSquare.tsx
@@ -1,8 +1,11 @@
-import React, { FunctionComponent } from 'react'
-import { Button } from 'react-bootstrap'
+import React, { FunctionComponent, useEffect } from 'react'
 import { useTree } from '../tree/TreeProvider'
+import { useSpring, a, config } from 'react-spring'
 
 type ResetSquareProps = {}
+
+// const defaultShadow = '0 0 20px 5px #006600'
+const defaultShadow = '0 0 0 0 #006600'
 
 const ResetSquare: FunctionComponent<ResetSquareProps> = ({}) => {
   /*******************************************************************************************************************
@@ -12,6 +15,14 @@ const ResetSquare: FunctionComponent<ResetSquareProps> = ({}) => {
    *******************************************************************************************************************/
 
   const { updateNode } = useTree()
+  const [styles, api] = useSpring(() => ({ boxShadow: '0 0 0 0 #0ff', transform: 'translate(0px, 0px)' }))
+
+  useEffect(() => {
+    api({
+      to: [{ transform: 'translate(0px, 0px)' }, { boxShadow: defaultShadow }],
+      config: { duration: 200 },
+    })
+  }, [])
 
   /*******************************************************************************************************************
    *
@@ -19,13 +30,24 @@ const ResetSquare: FunctionComponent<ResetSquareProps> = ({}) => {
    *
    *******************************************************************************************************************/
 
+  const handleClick = () => {
+    api({
+      to: [{ transform: `translate(0px, 0px) scale(0.9)` }, { transform: `translate(0px, 0px) scale(1)` }],
+      config: { ...config.gentle, duration: 200 },
+    })
+    updateNode([], { color: 'black' })
+  }
   /*******************************************************************************************************************
    *
    *  Rendering
    *
    *******************************************************************************************************************/
 
-  return <Button onClick={() => updateNode([], { color: 'black' })}>Reset</Button>
+  return (
+    <a.div style={styles} className='btn rounded-circle btn-dark' onClick={handleClick}>
+      <i className='bi bi-arrow-clockwise' />
+    </a.div>
+  )
 }
 
 export default ResetSquare
