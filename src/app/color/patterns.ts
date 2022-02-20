d7e7ced2b2ae699957303e7ef6c6f895d1fb22c6
Benedikt Lüken-Winkels <belu02@dfki.de>
2022-02-20 14:46:41 +0100

    Add IDs to Patterns

--- src/app/color/patterns.ts
+++ src/app/color/patterns.ts
@@ -4,15 +4,44 @@ export interface Pattern {
   c: { color: string }
   d: { color: string }
   single?: boolean
+  id: string
 }
 
 export const DefaultPatterns: Pattern[] = [
-  { a: { color: '#265728' }, b: { color: '#357a38' }, c: { color: '#459c48' }, d: { color: '#93cf95' } },
-  { a: { color: '#b36b00' }, b: { color: '#e68a00' }, c: { color: '#ffc266' }, d: { color: '#ffebcc' } },
-  { a: { color: '#cbb91a' }, b: { color: '#e8d84a' }, c: { color: '#f3eba5' }, d: { color: '#f9f5d2' } },
-  { a: { color: '#3e236d' }, b: { color: '#532e92' }, c: { color: '#845bcb' }, d: { color: '#c1ade5' } },
-  { a: { color: '#265728' }, b: { color: '#265728' }, c: { color: '#265728' }, d: { color: '#265728' }, single: true },
-  { a: { color: '#357a38' }, b: { color: '#357a38' }, c: { color: '#357a38' }, d: { color: '#357a38' }, single: true },
-  { a: { color: '#459c48' }, b: { color: '#459c48' }, c: { color: '#459c48' }, d: { color: '#459c48' }, single: true },
-  { a: { color: '#93cf95' }, b: { color: '#93cf95' }, c: { color: '#93cf95' }, d: { color: '#93cf95' }, single: true },
+  { id: 'green', a: { color: '#265728' }, b: { color: '#357a38' }, c: { color: '#459c48' }, d: { color: '#93cf95' } },
+  { id: 'orange', a: { color: '#b36b00' }, b: { color: '#e68a00' }, c: { color: '#ffc266' }, d: { color: '#ffebcc' } },
+  { id: 'khaki', a: { color: '#cbb91a' }, b: { color: '#e8d84a' }, c: { color: '#f3eba5' }, d: { color: '#f9f5d2' } },
+  { id: 'purple', a: { color: '#3e236d' }, b: { color: '#532e92' }, c: { color: '#845bcb' }, d: { color: '#c1ade5' } },
+  {
+    id: 'mono-green1',
+    a: { color: '#265728' },
+    b: { color: '#265728' },
+    c: { color: '#265728' },
+    d: { color: '#265728' },
+    single: true,
+  },
+  {
+    id: 'mono-green2',
+    a: { color: '#357a38' },
+    b: { color: '#357a38' },
+    c: { color: '#357a38' },
+    d: { color: '#357a38' },
+    single: true,
+  },
+  {
+    id: 'mono-green3',
+    a: { color: '#459c48' },
+    b: { color: '#459c48' },
+    c: { color: '#459c48' },
+    d: { color: '#459c48' },
+    single: true,
+  },
+  {
+    id: 'mono-green4',
+    a: { color: '#93cf95' },
+    b: { color: '#93cf95' },
+    c: { color: '#93cf95' },
+    d: { color: '#93cf95' },
+    single: true,
+  },
 ]
